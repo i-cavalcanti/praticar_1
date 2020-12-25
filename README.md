a411ac3b02db89ba16e6022e19d18351b8e26870
Mateus <mateus@organicbasics.com>
2020-12-25 16:12:55 +0100

    adds function to main and adds read me file

---
+++ README.md
@@ -0,0 +1,5 @@
+### Praticar 1
+Projeto para treinar como utilizar o git e github.
+
+### Run
+Run main.
--- main.py
+++ main.py
@@ -2,4 +2,13 @@ def main():
     print("Ivan")
     print("teste")
     print("new addition")
+
+
+def main_test():
+    for i in range(10):
+        print(i)
+
+
 main()
+
+main_test()
